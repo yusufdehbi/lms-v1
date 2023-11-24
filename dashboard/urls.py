9912ee37948f6fe37cf574426c4bbf565fe7c5f3
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-24 13:09:39 +0100

    ui for employess & leaves types

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,20 @@ from . import views
 urlpatterns = [
     path('', views.home_view),
     path('starter', views.starter_view),
+    path('test', views.test_view, name='test'),
+    # urls for admin
+
+    # -> dashboard
+    path('admin-dashboard', views.admin_dashboard_view),
+
+    # -> employee management
+    path('employee-management', views.employee_management_view, name="employee-management"),
+    path('add-employee/', views.add_employee_view, name='add-employee'),
+
+    # -> requests management
+    path('request-management', views.request_management, name='request-management'),
+    
+    # -> leaves policies management
+    path('leave-type-management', views.leave_type_management_view, name='leave-type-management'),
+    path('add-leave-type', views.add_leave_type_view, name='add-leave-type'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,3 +7,29 @@ def home_view(request):
 
 def starter_view(request):
     return render(request, 'starter.html')
+
+
+def admin_dashboard_view(request):
+    return render(request, 'admin dashboard.html')
+
+def employee_management_view(request):
+    return render(request, 'employee_management.html')
+
+def request_management(request):
+    return render(request, 'request_management.html')
+
+def leave_type_management_view(request):
+    return render(request,  'leave_type_management.html')
+
+# def add_leave_type_view(request):
+#     return render(request, 'add_leave_type.html')
+
+def add_leave_type_view(request):
+    return render(request, 'add_leave_type.html')
+
+def add_employee_view(request):
+    return render(request, 'add_employee.html')
+
+
+def test_view(request):
+    return render(request, 'test.html')
